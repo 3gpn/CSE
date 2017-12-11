b28149315e93a997e13a3f72e81a9d6f4e33e72a
3gpn <33762102+3gpn@users.noreply.github.com>
2017-12-11 10:46:33 -0800

    12/11/17

--- Madelynn Pattillo - Guessgame.py
+++ Madelynn Pattillo - Guessgame.py
@@ -1,22 +1,29 @@
 import random
 
+num_of_guesses = 5
 
-def compare(guess):
+
+def compare(guess, num_of_guesses):
     if guess < num:
-        print("Try again...but possibly higher than the last...")
+        num_of_guesses -= 1
+        print("Try again...but possibly higher than the last... You have %d guesses left." % num_of_guesses)
     elif guess > num:
-        print("That's incorrect...maybe something lower next time...")
+        num_of_guesses -= 1
+        print("That's incorrect...maybe something lower next time... You have %d guesses left." % num_of_guesses)
     else:
-        print("That's correct!")
+        num_of_guesses -= 1
+        print("That's correct! You had %d guesses left." % num_of_guesses)
 
 
 print("Welcome to Guessgame!")
 num = random.randint(1, 50)
 
 print()
-guess = input("Pick a number from 1 to 50.")
-compare(int(guess))
+
+if num_of_guesses == 0:
+    print("I'm sorry but that's incorrect. No more chances for you! Goodbye!")
 
 
-num_of_guess = int
-# 5. Add 5 guesses.
+while num_of_guesses <= 5:
+    guess = input("Pick a number from 1 to 50.")
+    compare(int(guess), num_of_guesses)
