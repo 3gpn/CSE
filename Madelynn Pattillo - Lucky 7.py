b2164c9041257be36b862d5fddf5536d0e6b02f2
3gpn <33762102+3gpn@users.noreply.github.com>
2018-01-11 10:46:24 -0800

    1/11/18

--- Madelynn Pattillo - Lucky 7.py
+++ Madelynn Pattillo - Lucky 7.py
@@ -1,13 +1,35 @@
 import random
 
+print("Welcome to Lucky 7s!")
 money = 15
-dice1 = random.randint(1, 6)
-dice2 = random.randint(1, 6)
+print("You are starting with $15.")
+rounds_played = 0
+most_money = 0
 
 while money > 0:
-    print("You know have")
-    print(dice1)
-    print(dice2)
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    rolled = dice1 + dice2
+    print("You bet $1.")
+    rounds_played += 1
+    money -= 1
+    print("You now have %d dollars left." % money)
+    print(rolled)
 
-if (dice1 + dice2) == 7:
-    print(money + 5)
+    if rolled == 7:
+        money += 5
+        print("You rolled a seven. You earn your bet plus an additional $4.")
+        print("You now have %d dollars left." % money)
+
+    if rolled != 7:
+        print("You lost the bet.")
+
+    if money >= most_money:
+        most_money = money - most_money
+
+    if money == 0:
+        print("Game over! You now have $0 and played a total of %d rounds." % rounds_played)
+
+
+
+# find greatest amount of money earned, round that you had the most money
