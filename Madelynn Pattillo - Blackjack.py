23ae270478ba304bc6c69d27938b4867e5e96fbe
3gpn <33762102+3gpn@users.noreply.github.com>
2018-01-17 10:45:51 -0800

    1/17/18

--- Madelynn Pattillo - Blackjack.py
+++ Madelynn Pattillo - Blackjack.py
@@ -1,53 +1,66 @@
 import random
 
 print("Welcome to Blackjack!")
-Dealer_card1 = random.randint(1, 13)
-Dealer_card2 = random.randint(1, 13)
-Player_card1 = random.randint(1, 13)
-Player_card2 = random.randint(1, 13)
+player_card1 = random.randint(1, 10)
+player_card2 = random.randint(1, 10)
+player_cards = player_card1 + player_card2
 blackjack = 21
-Player_cards = Player_card1 + Player_card2
-Dealer_cards = Dealer_card1 + Dealer_card2
 
 
-def winner(Dealer_cards, Player_cards):
-    if Dealer_cards > Player_cards and Dealer_cards < 21:
+def play(player_card1, player_card2, player_cards):
+    if play_choice == "hit":
+        player_card3 = random.randint(1, 10)
+        player_card4 = random.randint(1, 10)
+        player_cards = player_card1 + player_card2 + player_card3 + player_card4
+        print(player_card3)
+        print(player_card4)
+        print("Your cards total to %d." % player_cards)
+        dealer_card1 = random.randint(1, 10)
+        dealer_card2 = random.randint(1, 10)
+        dealer_cards = dealer_card1 + dealer_card2
+        print("The dealer's cards total to %d." % dealer_cards)
+    else:
+        dealer_card1 = random.randint(1, 10)
+        dealer_card2 = random.randint(1, 10)
+        dealer_cards = dealer_card1 + dealer_card2
+        print(dealer_card1)
+        print(dealer_card2)
+        dealer_cards = dealer_card1 + dealer_card2
+        print("The dealer's cards total to %d." % dealer_cards)
+
+
+def winner(dealer_cards, player_cards):
+    if dealer_cards > player_cards:
         print("You lost. The dealer won!")
-    elif Dealer_cards > Player_cards and Dealer_cards > 21:
+    elif dealer_cards > player_cards and dealer_cards > 21:
         print("You won!")
-    elif Player_cards > Dealer_cards and Player_cards < 21:
+    elif player_cards > dealer_cards:
         print("You won!")
-    elif Player_cards > Dealer_cards and Player_cards > 21:
+    elif player_cards > dealer_cards and player_cards > 21:
         print("You lost. The dealer won!")
-    elif Dealer_cards == blackjack and Player_cards != blackjack:
+    elif dealer_cards == blackjack and player_cards != blackjack:
         print("You lost. The dealer won!")
-    elif Player_cards == blackjack and Dealer_cards != blackjack:
+    elif player_cards == blackjack and dealer_cards != blackjack:
         print("You won!")
-    elif Player_cards == Dealer_cards:
+    elif player_cards == dealer_cards:
         print("It's a draw.")
-    elif Player_cards == blackjack:
+    elif player_cards == blackjack:
         print("You won!")
-    elif Dealer_cards == blackjack:
+    elif dealer_cards == blackjack:
         print("You lost. The dealer won!")
 
 
-while Player_cards < 21:
-    print(Player_card1)
-    print(Player_card2)
-    print("Your cards total to %d." % Player_cards)
+while player_cards <= 21:
+    player_card1 = random.randint(1, 10)
+    player_card2 = random.randint(1, 10)
+    player_cards = player_card1 + player_card2
+    dealer_card1 = random.randint(1, 10)
+    dealer_card2 = random.randint(1, 10)
+    dealer_cards = dealer_card1 + dealer_card2
+    print(player_card1)
+    print(player_card2)
+    print("Your cards total to %d." % player_cards)
     play_choice = input("Do you want to stand or hit on your cards?")
     print(play_choice)
-
-    if play_choice == "hit":
-        Player_card3 = random.randint(1, 13)
-        Player_card4 = random.randint(1, 13)
-        Player_cards = Player_card3 + Player_card4
-        print(Player_card3)
-        print(Player_card4)
-        print("Your cards total to %d." % Player_cards)
-    else:
-        print(Dealer_card1)
-        print(Dealer_card2)
-        Dealer_cards = Dealer_card1 + Dealer_card2
-        print("The dealer's cards total to %d." % Dealer_cards)
-        print(winner(Dealer_cards, Player_cards))
+    print(play(player_card1, player_card2, player_cards))
+    print(winner(dealer_cards, player_cards))
