780e36a8681a9d0bfe314424d6b67affd531821f
3gpn <33762102+3gpn@users.noreply.github.com>
2018-01-22 10:46:37 -0800

    1/22/18

--- Madelynn Pattillo - Hangman.py
+++ Madelynn Pattillo - Hangman.py
@@ -1,14 +1,17 @@
 import random
 import string
 
-word_list = "Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledore", "Severus Snape", "Remus Lupin", \
-            "Fred and George Weasley", "Neville Longbottom", "Xenophilius Lovegood", "Lord Voldemort"
+print("Welcome to Hangman: Harry Potter Edition!")
+word_list = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledore", "Severus Snape", "Remus Lupin",
+            "Fred and George Weasley", "Neville Longbottom", "Xenophilius Lovegood", "Lord Voldemort"]
 
+random_word = random.choice(word_list)
+print(*random_word)
 """
 
 A general guide for Hangman
 1. Make a word bank - 10 items (DONE)
-2. Pick an item from the list
+2. Pick an item from the list (DONE)
 3. Hide the word (use *)
 4. Reveal letters already guessed
 5. Create the win condition
