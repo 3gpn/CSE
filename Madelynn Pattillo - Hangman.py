d5c0dab8280aa848296b3c0ee5dca62ba0a4f215
3gpn <33762102+3gpn@users.noreply.github.com>
2018-01-24 10:45:37 -0800

    1/24/18

--- Madelynn Pattillo - Hangman.py
+++ Madelynn Pattillo - Hangman.py
@@ -3,24 +3,33 @@ import string
 
 print("Welcome to Hangman: Harry Potter Edition!")
 guesses_left = 10
-letters_guessed = []
+letters_guessed = [' ']
+alphabet = string.ascii_letters
 word_list = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledore", "Severus Snape", "Remus Lupin",
             "Fred and George Weasley", "Neville Longbottom", "Xenophilius Lovegood", "Lord Voldemort"]
 random_word = random.choice(word_list)
 
 while guesses_left > 0:
-    guess = input("Guess a letter from A to Z. You have %d guesses left." % guesses_left)
+    output = []
+    for letter in random_word:
+        if letter in letters_guessed:
+            output.append(letter)
+        else:
+            output.append("*")
+    print(output)
+
+    if guess != letter in random_word:
+        guesses_left -= 1
+
+    guess = input("Guess a letter from the alphabet. You have %d guesses left." % guesses_left)
     letters_guessed.append(guess)
     print("You have guessed %s." % letters_guessed)
-    guesses_left -= 1
 
-"""
+    if output == random_word:
+        print("Congradulations! You guessed the correct word: %s." % list(output))
+    else:
+        print("You lost the word is %s." % random_word)
 
-A general guide for Hangman
-1. Make a word bank - 10 items (DONE)
-2. Pick an item from the list (DONE)
-3. Add a guess to the list of letters guessed (DONE)
-4. Reveal letters already guessed
-5. Create the win condition
 
-"""
+
+# 5. Create the win condition
