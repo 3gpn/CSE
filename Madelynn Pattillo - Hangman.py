092852b720ee3d1531bb066f436e71e8adf0583e
3gpn <33762102+3gpn@users.noreply.github.com>
2018-01-30 10:47:11 -0800

    1/30/18

--- Madelynn Pattillo - Hangman.py
+++ Madelynn Pattillo - Hangman.py
@@ -9,25 +9,29 @@ word_list = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Albus Dumbledor
             "Fred and George Weasley", "Neville Longbottom", "Xenophilius Lovegood", "Lord Voldemort"]
 random_word = random.choice(word_list)
 
-while guesses_left > 0:
+while guesses_left >= 0:
     output = []
     for letter in random_word:
         if letter in letters_guessed:
+            output.append(letter)
+        elif letter.lower() in letters_guessed:
+            output.append(letter)
+        elif letter.upper() in letters_guessed:
             output.append(letter)
         else:
             output.append("*")
     print(output)
     guessed_word = "".join(output)
     guess = input("Guess a letter from the alphabet. You have %d guesses left." % guesses_left)
+    letters_guessed.append(guess)
 
-    if guess.lower() in random_word and alphabet:
-        output.append(guess.lower())
+    if guess not in random_word:
         guesses_left -= 1
 
-    letters_guessed.append(guess)
     print("You have guessed %s." % letters_guessed)
 
-    # if random_word == guessed_word:
-    #     exit(0)
-    #     print("Game over!")
+    if random_word == guessed_word:
+        print("Game over!")
+        exit(0)
+    else:
 # 5. Create the win condition
