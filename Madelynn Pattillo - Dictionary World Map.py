63bfa6895d53111d117c0a7cdbd7d01a413f3cd5
3gpn <33762102+3gpn@users.noreply.github.com>
2018-02-08 10:46:40 -0800

    2/8/18

--- Madelynn Pattillo - Dictionary World Map.py
+++ Madelynn Pattillo - Dictionary World Map.py
@@ -1,3 +1,4 @@
+# Example:
 world_map = {
     'WESTHOUSE': {
         'NAME': "West of House",
@@ -20,5 +21,43 @@ world_map = {
         'PATHS': {
             'NORTH': 'WESTHOUSE'
         }
+    }
+}
+
+Hogwarts_Campus = {
+    'CHAMBER': {
+        'NAME': "Chamber of Secrets",
+        'DESCRIPTION': "You are below the dungeons",
+        'PATHS': {
+            'UP': 'DUNGEONS'
+        }
+    },
+    'DUNGEONS': {
+        'NAME': "The Dungeons",
+        'DESCRIPTION': "You are below the castle",
+        'PATHS': {
+            'WEST': 'POTIONS',
+            'EAST': 'SLYTHERIN',
+            'UP': 'LEVEL 1'
+        }
+    },
+    'POTIONS': {
+        'NAME': "Potion's Classroom",
+        'DESCRIPTION': "You are in one of the bigger dungeons below the castle",
+        'PATHS': {
+            'WEST': 'SNAPE',
+            'EAST': 'DUNGEONS'
+        }
+    },
+    'SNAPE': {
+        'NAME': "Snape's Office",
+        'DESCRIPTION': "You are in a small, dark room adjacent to the Potion's Classroom",
+        'PATHS': {
+            'EAST': 'POTIONS'
+        }
+    },
+    'SLYTHERIN': {
+        'NAME': "Slytherin Common Room",
+        'DESCRIPTION': "You are in the living quarters of the Slytherins of Hogwarts"
     }
 }
