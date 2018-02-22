50cfb102b0105310c649d1b75e31297563d912b4
3gpn <33762102+3gpn@users.noreply.github.com>
2018-02-22 10:46:34 -0800

    2/22/18

---
+++ Madelynn Pattillo - World Map OOP.py
@@ -0,0 +1,11 @@
+class Room(object):
+    def __init__(self, name, description, up, down, north, south, east, west):
+        self.name = name
+        self.description = description
+        self.up = up
+        self.down = down
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+
---
+++ Pythagorean Theorem.py
@@ -0,0 +1,7 @@
+def pythagorean(a, b):
+    c = a ** 2 + b ** 2
+    answer = c ** 0.5
+    return answer
+
+
+print(pythagorean(5, 12))
